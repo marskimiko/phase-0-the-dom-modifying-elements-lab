3ac5ab8e4bcd38967953728bebf47d2c66c87438
marisasaunders <marisaksaunders@gmail.com>
2021-12-31 14:55:14 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const mainElemnt = document.querySelector("#main")//.remove();
+
+main.remove()
+
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory'
+
+newHeader.innerHTML = "Marisa is the champion!"
+
+document.body.append(newHeader);
+
